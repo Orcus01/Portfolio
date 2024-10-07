aaec6cf20f51bbfda8e9a6aa43f812bb137379a1
Japneet Singh <japneetsingh@Orcus.local>
2024-10-07 17:01:53 +0530

    first commit

---
+++ src/assets/index.js
@@ -0,0 +1,101 @@
+import japneetsingh from "./japneet.svg";
+import profilePic from "./profile-pic.jpeg";
+import quotes from "./quotes.svg";
+import robot from "./robot.png";
+import send from "./Send.svg";
+import shield from "./Shield.svg";
+import star from "./Star.svg";
+import menu from "./menu.svg";
+import close from "./close.svg";
+import google from "./google.svg";
+import apple from "./apple.svg";
+import arrowUp from "./arrow-up.svg";
+import discount from "./Discount.svg";
+import facebook from "./facebook.svg";
+import instagram from "./instagram.svg";
+import linkedin from "./linkedin.svg";
+import twitter from "./twitter.svg";
+import cu from "./cu.png";
+import cluboard from "./cluboard.png";
+import cash_flow from "./cash_flow.jpg";
+import freadom from "./freadom.jpg";
+import ecell from "./ecell.png";
+import bank from "./bank.jpg";
+import gdsc from "./gdsc.png";
+import ieee from "./ieee.jpeg";
+import genesis from "./genesis.jpg";
+import todo from "./todo.jpg";
+import graphql from "./graphql.png";
+import portfolio from "./portfolio.jpg";
+import zulip from "./zulip.jpg";
+import publiclab from "./publiclab.png";
+import cdc from "./cdc.png";
+import chargeswap from "./chargeswap.png";
+import placeicon from "./place-icon.svg";
+import recruitment from "./recruitment.png";
+import huntly from "./huntly.png";
+import oracle from "./oracle.jpg";
+import comicify_ai from "./comicify-ai.png";
+import greentrust from "./green_trust.png";
+import grownityin_logo from "./grownityin_logo.jpeg";
+import devfolio from "./devfolio.png";
+import srap from "./srap.png";
+import ethglobal from "./ethglobal.png";
+import polkadot from "./polkadot.jpeg";
+import lightspeed from "./lightspeed.png";
+import dennisivy from "./dennisivy.png";
+import manipal from "./manipal.png";
+import icon from "./icon.jpg";
+import ethforall from "./ethforall.png";
+
+export {
+  quotes,
+  robot,
+  send,
+  shield,
+  star,
+  menu,
+  close,
+  google,
+  apple,
+  arrowUp,
+  discount,
+  facebook,
+  instagram,
+  linkedin,
+  twitter,
+  cu,
+  cluboard,
+  cash_flow,
+  freadom,
+  bank,
+  gdsc,
+  ieee,
+  ecell,
+  genesis,
+  todo,
+  graphql,
+  portfolio,
+  publiclab,
+  zulip,
+  cdc,
+  japneetsingh,
+  profilePic,
+  chargeswap,
+  placeicon,
+  recruitment,
+  huntly,
+  oracle,
+  comicify_ai,
+  greentrust,
+  grownityin_logo,
+  devfolio,
+  srap,
+  ethglobal,
+  polkadot,
+  lightspeed,
+  dennisivy,
+  manipal,
+  icon,
+  ethforall
+};
---
+++ src/constants/index.js
@@ -0,0 +1,881 @@
+import {
+  cu,
+  cluboard,
+  cash_flow,
+  gdsc,
+  ieee,
+  ecell,
+  genesis,
+  portfolio,
+  publiclab,
+  zulip,
+  cdc,
+  chargeswap,
+  placeicon,
+  recruitment,
+  huntly,
+  comicify_ai,
+  greentrust,
+  grownityin_logo,
+  devfolio,
+  srap,
+  ethglobal,
+  polkadot,
+  lightspeed,
+  dennisivy,
+  manipal,
+  icon,
+  ethforall
+} from "../assets";
+
+import {
+  AiFillGithub,
+  AiFillInstagram,
+  AiFillLinkedin,
+  AiFillMail,
+  AiOutlineTwitter,
+  AiFillHtml5,
+  AiOutlineGitlab,
+} from "react-icons/ai";
+
+import {
+  SiDjango,
+  SiJavascript,
+  SiBootstrap,
+  SiReact,
+  SiTailwindcss,
+  SiGraphql,
+  SiPython,
+  SiCplusplus,
+  SiC,
+  SiRubyonrails,
+  SiJquery,
+  SiVisualstudiocode,
+  SiPostman,
+  SiGit,
+  SiMysql,
+  SiSolidity,
+  SiNetlify,
+  SiChartdotjs,
+  SiVite,
+  SiArduino,
+  SiWeb3Dotjs,
+  SiIpfs,
+  SiDotnet,
+  SiTwilio,
+  SiFlutter,
+  SiReplit,
+  SiFlask,
+  SiFigma,
+  SiGooglemaps,
+  SiOpenai,
+  SiGooglecloud,
+  SiNextdotjs,
+  SiMicrosoftazure,
+  SiDart,
+  SiFirebase,
+  SiNodedotjs
+} from "react-icons/si";
+
+import {
+  FaHardHat,
+  FaNodeJs,
+  FaRust
+} from "react-icons/fa";
+
+import {
+  IoIosNotificationsOutline
+} from "react-icons/io";
+
+import {
+  FaGolang
+} from "react-icons/fa6";
+
+import { DiCss3, DiJava, DiMsqlServer, DiRuby } from "react-icons/di";
+
+export const resumeLink = "https://drive.google.com/file/d/1vkxyMDB5_KpMwt4QXFgT2aqdRizr8Czh/view?usp=sharing";
+export const repoLink = "https://github.com/mittal-parth/personal-portfolio";
+
+export const callToAction = "https://www.linkedin.com/in/japneet--singh";
+
+export const navLinks = [
+  {
+    id: "skills",
+    title: "Skills & Experience",
+  },
+  {
+    id: "education",
+    title: "Education",
+  },
+  {
+    id: "achievements",
+    title: "Achievements",
+  },
+  {
+    id: "projects",
+    title: "Projects",
+  },
+  {
+    id: "extraCurricular",
+    title: "Extra Curricular",
+  },
+  {
+    id: "contactMe",
+    title: "Contact Me",
+  },
+];
+
+export const educationList = [
+  {
+    id: "education-1",
+    icon: cu,
+    title: "Chandigarh University, Mohali, Punjab",
+    degree: "Bachelor of Engineering",
+    duration: "August 2021 - June 2025",
+    content1: "Computer Science and Engineering",
+    content2: "CGPA: 7.98",
+  },
+  {
+    id: "education-2",
+    icon: srap,
+    title: "Shri Ram Ashram Public School",
+    degree: "Intermediate",
+    duration: "2020 - 2021",
+    content1: "Percentage: 95.4",
+  },
+];
+
+export const achievements = [
+  {
+    id: "a-1",
+    icon: ethglobal,
+    event: "ETHIndia'22 | World's Largest Ethereum Hackathon",
+    position: "Winner",
+    content1: "Top 12 winners among 20k+ registrations",
+    content2: "One of Polygon's Best Public Goods",
+    content3: "Best Module on Biconomy SDK",
+    article: "https://www.thehindu.com/news/cities/Mangalore/cu-iiit-delhi-team-makes-it-to-top-12-winners-in-ethindia-22/article66238923.ece",
+    project: "https://devfolio.co/projects/chargeswap-3527",
+    youtube: "https://youtu.be/9rieTya8Yds?t=3908",
+  },
+  {
+    id: "a-2",
+    icon: polkadot,
+    event: "Polkadot Hackathon: Europe Edition",
+    position: "2nd Runner Up in the ink! Smart Contract Category",
+    content1: "Built GreenTrust offering a novel solution for obtaining certification in organic farming.",
+    content2: "",
+    content3: "",
+    github: "https://github.com/pranav2305/GreenTrust",
+  },
+  {
+    id: "a-3",
+    icon: lightspeed,
+    event: "Warpspeed by Lightspeed",
+    position: "1st Runner Up",
+    content1: "1st Runner Up Overall by Lightspeed among 107 hackers",
+    content2: "Top 3 projects using Replit",
+    content3: "1st Runner Up by Amazon Web Services (AWS)",
+    article: "https://shorturl.at/fhjsT",
+  },
+  {
+    id: "a-4",
+    icon: dennisivy,
+    event: "September Hackathon by Dennis Ivy",
+    position: "Winner",
+    content1: "Rated the best portfolio website among 450+ participants across the globe.",
+    content2: "",
+    content3: "",
+    youtube: "https://www.youtube.com/watch?v=X2473En3h_o&t=5278s",
+    project: "https://japneetsingh.netlify.app/",
+  },
+  {
+    id: "a-5",
+    icon: manipal,
+    event: "Manipal Hackathon'22",
+    position: "Consolation Prize",
+    content1: "Top 10 among 500+ teams across India",
+    content2: "Developed a cross-platform mobile application to address the problem of social cohesion.",
+    content3: "",
+    article: "https://shorturl.at/exEIQ",
+  },
+  {
+    id: "a-6",
+    icon: icon,
+    event: "ICON Hyperbuild Hackathon",
+    position: "Honorable Mention",
+    content1: "Honorable Mention among 655 participants across the globe in a 3 month-long online hackathon.",
+    content2: "",
+    content3: "",
+    project: "https://devpost.com/software/green-trust-xj2w6g",
+  },
+  {
+    id: "a-7",
+    icon: ethforall,
+    event: "ETHForAll 2023",
+    position: "Top 3 Superfluid Projects",
+    content1: "Bounty winners among 430 projects in ETHGlobal's largest online hackathon.",
+    content2: "",
+    content3: "",
+    project: "https://devfolio.co/projects/green-trust-ed14",
+  },
+];
+
+
+export const skills = [
+  {
+    title: "Programming Languages",
+    items: [
+      {
+        id: "pl-2",
+        icon: SiPython,
+        name: "Python",
+      },
+      {
+        id: "pl-3",
+        icon: SiCplusplus,
+        name: "C++",
+      },
+      {
+        id: "pl-4",
+        icon: SiDart,
+        name: "Dart",
+      },
+      {
+        id: "pl-6",
+        icon: DiJava,
+        name: "Java",
+      },
+      {
+        id: "pl-7",
+        icon: SiC,
+        name: "C",
+      },
+      {
+        id: "pl-8",
+        icon: AiFillHtml5,
+        name: "HTML",
+      },
+      {
+        id: "pl-9",
+        icon: DiCss3,
+        name: "CSS",
+      },
+      {
+        id: "pl-10",
+        icon: SiJavascript,
+        name: "JavaScript",
+      },
+    ],
+  },
+  {
+    title: "Frameworks/Libraries",
+    items: [
+      {
+        id: "f-1",
+        icon: SiFlutter,
+        name: "Flutter",
+      },
+      {
+        id: "f-2",
+        icon: FaNodeJs,
+        name: "NodeJS",
+      },
+      {
+        id: "f-3",
+        icon: SiReact,
+        name: "ReactJS",
+      },
+      {
+        id: "f-4",
+        icon: SiBootstrap,
+        name: "Bootstrap",
+      },
+      {
+        id: "f-5",
+        icon: SiTailwindcss,
+        name: "Tailwind CSS",
+      },
+      {
+        id: "f-6",
+        icon: SiJquery,
+        name: "jQuery",
+      },
+      {
+        id: "f-7",
+        icon: SiGraphql,
+        name: "GraphQL",
+      },
+      {
+        id: "f-8",
+        icon: SiDotnet,
+        name: ".NET",
+      },
+    ],
+  },
+  {
+    title: "Tools",
+    items: [
+      {
+        id: "t-1",
+        icon: SiMicrosoftazure,
+        name: "Azure",
+      },
+      {
+        id: "t-2",
+        icon: SiMysql,
+        name: "MySQL",
+      },
+      {
+        id: "t-4",
+        icon: SiVisualstudiocode,
+        name: "VS Code",
+      },
+      {
+        id: "t-5",
+        icon: SiGit,
+        name: "Git",
+      },
+      {
+        id: "t-6",
+        icon: AiFillGithub,
+        name: "GitHub",
+      },
+      {
+        id: "t-7",
+        icon: AiOutlineGitlab,
+        name: "Gitlab",
+      },
+      {
+        id: "t-8",
+        icon: SiNetlify,
+        name: "Netlify",
+      },
+      {
+        id: "t-9",
+        icon: SiVite,
+        name: "ViteJS",
+      },
+    ],
+  },
+];
+
+export const experiences = [
+  {
+    organisation: "Grownity",
+    logo: grownityin_logo,
+    link: "https://play.google.com/store/apps/details?id=com.Grownity.app",
+    positions: [
+      {
+        title: "Application Developer Intern",
+        duration: "Sept 2023 - Feb 2024",
+        content: [
+          {
+            text: "Developed the application for Grownity using Flutter and Dart, also worked on the webpage along with team.",
+            link: ""
+          },
+          {
+            text: "Designed the FrontEnd of the application.",
+            link: ""
+          }
+        ],
+      },
+    ],
+  },
+  {
+    organisation: "IEEE, CU",
+    logo: ieee,
+    link: "https://ieee.cu.ac.in/about_us",
+    positions: [
+      {
+        title: "Vice Chairperson - IEEE PES CUSB",
+        duration: "Apr 2024 - Present",
+        content: [
+          {
+            text: "Led the team of IEEE PES CUSB as the Vice Chairperson ",
+            link: "",
+          },
+          {
+            text: "Managed all events and responsibilities.",
+            link: "",
+          },
+        ],
+      },
+      {
+        title: "Secretary - IEEE COMPUTER SOCIETY CUSB",
+        duration: "Aug 2023 - Apr 2024",
+        content: [
+          {
+            text: "Managed the responsibility of documentation and organizing different events.",
+            link: "",
+          },
+        ],
+      },
+      {
+        title: "Web Developer - IEEE Photonics Society CUSB",
+        duration: "Apr 2023 - Aug 2023",
+        content: [
+          {
+            text: "Worked on developing and mangaing the Photonics Society's Webpage.",
+            link: "",
+          },
+        ],
+      },
+      {
+        title: "Reviewer - IEEE Richard E Merwin Scholarship",
+        duration: "Fall Cycle - 2023",
+        content: [
+          {
+            text: "Worked under the IEEE REM Scholarship team as a reviewer for the applicant profiles.",
+            link: "",
+          },
+        ],
+      },
+    ],
+  }
+];
+
+export const openSourceContributions = [
+
+];
+
+export const projects = [
+  {
+    id: "project-1",
+    title: "Kissan Konnect",
+    github: "https://github.com/ayush4345/Comicify.ai",
+    link: "https://comicify-ai.vercel.app/",
+    image: greentrust,
+    content:
+      "Connects Farmers and other Plant Producers to detect plant diseases and give the cure for them.",
+    stack: [
+      {
+        id: "icon-1",
+        icon: SiFlutter,
+        name: "Flutter"
+      },
+      {
+        id: "icon-2",
+        icon: SiDart,
+        name: "Dart"
+      },
+      {
+        id: "icon-3",
+        icon: SiOpenai,
+        name: "OpenAI"
+      },
+      {
+        id: "icon-4",
+        icon: SiFirebase,
+        name: "Firebase"
+      },
+      {
+        id: "icon-5",
+        icon: SiFigma,
+        name: "Figma"
+      },
+    ],
+  },
+  {
+    id: "project-2",
+    title: "webUS Builderz",
+    github: "https://github.com/mittal-parth/GreenTrust",
+    link: "https://green-trust-fantom.netlify.app/",
+    image: greentrust,
+    content:
+      "webUS Builderz is a series of projects that include Online Resume and Portfolio Builder and an E-commerce website.",
+    stack: [
+      {
+        id: "icon-1",
+        icon: SiReact,
+        name: "React"
+      },
+      {
+        id: "icon-2",
+        icon: SiTailwindcss,
+        name: "TailwindCSS"
+      },
+      {
+        id: "icon-3",
+        icon: SiNextdotjs,
+        name: "Next.js"
+      },
+      {
+        id: "icon-4",
+        icon: SiFigma,
+        name: "Figma"
+      },
+      {
+        id: "icon-5",
+        icon: SiNodedotjs,
+        name: "Node"
+      },
+    ],
+  },
+  {
+    id: "project-3",
+    title: "ParkCar",
+    github: "https://github.com/CommanderAstern/ChargeSwap",
+    link: "https://devfolio.co/projects/chargeswap-3527",
+    image: chargeswap,
+    content:
+        "ParkCar is a automated car parking space app that uses realtime parking manager and provides integrated payment's gateway",
+    stack: [
+      {
+        id: "icon-1",
+        icon: SiFlutter,
+        name: "Flutter"
+      },
+      {
+        id: "icon-2",
+        icon: SiDart,
+        name: "Dart"
+      },
+      {
+        id: "icon-3",
+        icon: SiOpenai,
+        name: "OpenAI"
+      },
+      {
+        id: "icon-4",
+        icon: SiFirebase,
+        name: "Firebase"
+      },
+      {
+        id: "icon-5",
+        icon: SiFigma,
+        name: "Figma"
+      },
+    ],
+  },
+  {
+    id: "project-4",
+    title: "Raahi",
+    github: "https://github.com/Orcus01/Raahi",
+    link: "https://raahi-esp.netlify.app",
+    image: placeicon,
+    content:
+      "Raahi is a webpage that help college seniors placed in differnet companies to connect and guide their juniors by providing study materials and tips.",
+    stack: [
+      {
+        id: "icon-1",
+        icon: SiReact,
+        name: "React"
+      },
+      {
+        id: "icon-2",
+        icon: SiTailwindcss,
+        name: "TailwindCSS"
+      },
+      {
+        id: "icon-3",
+        icon: SiNextdotjs,
+        name: "Next.js"
+      },
+      {
+        id: "icon-4",
+        icon: SiFigma,
+        name: "Figma"
+      },
+      {
+        id: "icon-5",
+        icon: SiNodedotjs,
+        name: "Node"
+      },
+    ],
+  },
+  {
+    id: "project-5",
+    title: "Uniride",
+    github: "",
+    link: "http://recruitment.cu.ac.in/",
+    image: recruitment,
+    content:
+      "Uniride is a car pooling system that allows different teachers and students to pool their cars and bikes vacant seats and earn money.",
+    stack: [
+      {
+        id: "icon-1",
+        icon: SiReact,
+        name: "React"
+      },
+      {
+        id: "icon-2",
+        icon: SiTailwindcss,
+        name: "TailwindCSS"
+      },
+      {
+        id: "icon-3",
+        icon: SiNextdotjs,
+        name: "Next.js"
+      },
+      {
+        id: "icon-4",
+        icon: SiFigma,
+        name: "Figma"
+      },
+      {
+        id: "icon-5",
+        icon: SiNodedotjs,
+        name: "Node"
+      },
+    ],
+  },
+  {
+    id: "project-6",
+    title: "Evento",
+    github: "",
+    link: "http://cdc.cu.ac.in/",
+    image: cdc,
+    content:
+      "Evento is an event publishing website that helps different socities to publish, manage and organize their events and also bring participation through community portal and even take payments",
+    stack: [
+      {
+        id: "icon-1",
+        icon: SiReact,
+        name: "React"
+      },
+      {
+        id: "icon-2",
+        icon: SiTailwindcss,
+        name: "TailwindCSS"
+      },
+      {
+        id: "icon-3",
+        icon: SiNextdotjs,
+        name: "Next.js"
+      },
+      {
+        id: "icon-4",
+        icon: SiFigma,
+        name: "Figma"
+      },
+      {
+        id: "icon-5",
+        icon: SiNodedotjs,
+        name: "Node"
+      },
+    ],
+  },
+  {
+    id: "project-7",
+    title: "Portfolio",
+    github: "https://github.com/mittal-parth/personal-portfolio",
+    link: "https://japneetsingh.netlify.app/",
+    image: portfolio,
+    content: "Personal portfolio website with React and Tailwind CSS.",
+    stack: [
+      {
+        id: "icon-1",
+        icon: SiReact,
+        name: "React"
+      },
+      {
+        id: "icon-2",
+        icon: SiTailwindcss,
+        name: "Tailwind CSS"
+      },
+      {
+        id: "icon-3",
+        icon: AiFillHtml5,
+        name: "HTML"
+      },
+    ],
+  },
+];
+
+export const blogPosts = [
+  {
+    id: "post-1",
+    title: "Blog Post 01 - Title",
+    link: "#",
+    date: new Date().toLocaleDateString(), // Can be edited to any string format
+    image: "https://via.placeholder.com/600/92c952",
+    tags: [
+      {
+        id: "tag-1",
+        name: "tag 01"
+      },
+      {
+        id: "tag-2",
+        name: "tag 03"
+      },
+      {
+        id: "tag-3",
+        name: "tag 03"
+      },
+    ],
+  },
+  {
+    id: "post-2",
+    title: "Blog Post 02 - Title",
+    link: "#",
+    date: new Date().toLocaleDateString(),
+    image: "https://via.placeholder.com/600/d32776",
+    tags: [
+      {
+        id: "tag-1",
+        name: "tag 01"
+      },
+      {
+        id: "tag-2",
+        name: "tag 03"
+      },
+      {
+        id: "tag-3",
+        name: "tag 03"
+      },
+    ],
+  },
+  {
+    id: "post-3",
+    title: "Blog Post 03 - Title",
+    link: "#",
+    date: new Date().toLocaleDateString(),
+    image: "https://via.placeholder.com/600/771796",
+    tags: [
+      {
+        id: "tag-1",
+        name: "tag 01"
+      },
+      {
+        id: "tag-2",
+        name: "tag 03"
+      },
+      {
+        id: "tag-3",
+        name: "tag 03"
+      },
+    ],
+  },
+];
+
+export const stats = [
+  {
+    id: "stats-1",
+    title: "Organisations",
+    value: "2+",
+  },
+  {
+    id: "stats-2",
+    title: "Issues Opened",
+    value: "6+",
+  },
+  {
+    id: "stats-3",
+    title: "Pull Requests",
+    value: "6+",
+  },
+];
+
+export const extraCurricular = [
+  {
+    organisation: "Devfolio",
+    title: "UniDAO Lead",
+    duration: "December 2021 - Present",
+    content: [
+      {
+        text: "Selected among 5 students across the country to lead the initiative and grow the culture of Blockchain and Ethereum, powered by Devfolio.",
+        link: "https://www.linkedin.com/feed/update/urn:li:activity:7097977924686942209/",
+      },
+      {
+        text: "Led a cohort of 37 selected students over 6 weeks to learn and build in the Ethereum ecosystem.",
+        link: "https://www.linkedin.com/feed/update/urn:li:activity:7095310520282480641/",
+      },
+    ],
+    logo: devfolio,
+  },
+  {
+    organisation: "Google Developer Student Club, cu",
+    title: "Co-Chair",
+    duration: "December 2021 - Present",
+    content: [
+      {
+        text: "Started HackClub to promote and spread the culture of Hackathons in the college. 20+ hackathons particiaptions, 15+ wins over the year.",
+        link: "",
+      },
+      {
+        text: "Co-designed and developed the official website of Incident, cu with 15K+ visitors",
+        link: "https://incident.cu.ac.in/",
+      },
+    ],
+    logo: gdsc,
+  },
+  {
+    organisation: "Genesis, cu",
+    title: "Competitions Head",
+    duration: "Sep 2021 - Present",
+    content: [
+      {
+        text: "Qualified for the nationals of Indian Hip Hop Dance Championship",
+        link: "",
+      },
+      {
+        text: "Won 7 inter-college solo dance competitions",
+        link: "",
+      },
+    ],
+    logo: genesis,
+  },
+  {
+    organisation: "ieee, cu",
+    title: "Tutor",
+    duration: "Jan 2022 - Jan 2022",
+    content: [
+      {
+        text: "Mentored 150+ students in a month-long Web Development and Ruby on Rails Bootcamp",
+        link: "https://github.com/ieee-cu/ieee-RoR-Bootcamp-2021",
+      },
+    ],
+    logo: ieee,
+  },
+  {
+    organisation: "E-Cell, cu",
+    title: "Executive Member",
+    duration: "Sep 2021 - Apr 2022",
+    content: [
+      {
+        text: "Organised the season 3 of the E-Cell cu Podcast",
+        link: "https://www.youtube.com/watch?v=uA-Yrk6bVDc",
+      },
+      {
+        text: "Member of the Sponsorship and E-Talks team for E-Summit'22",
+        link: "",
+      },
+    ],
+    logo: ecell,
+  },
+];
+
+export const socialMedia = [
+  {
+    id: "social-media-1",
+    icon: AiFillLinkedin,
+    link: "https://www.linkedin.com/in/japneet--singh/",
+  },
+  {
+    id: "social-media-2",
+    icon: AiFillGithub,
+    link: "https://github.com/Orcus01",
+  },
+  {
+    id: "social-media-3",
+    icon: AiFillMail,
+    link: "mailto:japneet.singh2024@gmail.com",
+  },
+  {
+    id: "social-media-4",
+    icon: AiOutlineTwitter,
+    link: "https://x.com/Orcus001",
+  },
+  {
+    id: "social-media-5",
+    icon: AiFillInstagram,
+    link: "https://www.instagram.com/japneetsingh2003/",
+  },
+];
+
+export const aboutMe = {
+    name: "Japneet Singh",
+    tagLine: "Software Developer | Vice Chairperson - IEEE PES Society CUSB | Orcus",
+    intro: "Software Developer from India who is either busy improving his craft or pondering over the next big idea."
+}
